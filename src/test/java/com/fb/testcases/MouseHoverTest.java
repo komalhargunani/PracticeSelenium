dab25971d7a222f3d287021a5d1273ab391908a2
komalh <komalh@ULTP_649.synerzipune.local>
2017-07-19 14:06:39 +0530

    adding

--- src/test/java/com/fb/testcases/MouseHoverTest.java
+++ src/test/java/com/fb/testcases/MouseHoverTest.java
@@ -1,17 +1,10 @@
 package com.fb.testcases;
 
-import java.util.Iterator;
 import java.util.Properties;
-import java.util.Set;
-
-import org.apache.poi.util.SystemOutLogger;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -52,25 +45,7 @@ public class MouseHoverTest {
 		
 		driver.findElement(By.xpath("//a[@data-ga-track='Main Navigation Jobs|Jobs by Skill']")).click();
 		  
-	/*	// Type something on Skill textbox
-		driver.findElement(By.name("qp")).sendKeys("test");
-		 
-		// Create object on Actions class
-		Actions builder=new Actions(driver);
-		 
-		// find the element which we want to Select from auto suggestion
-		WebElement ele=driver.findElement(By.xpath(".//*[@id='autosuggest']/ul/li[2]/a"));
-		 
-		// use Mouse hover action for that element
-		builder.moveToElement(ele).build().perform();
-		 
-		// Give wait for 2 seconds 
-		Thread.sleep(2000);
-		 
-		// finally click on that element
-		builder.click(ele).build().perform();*/
-		
-		
+	
 		
 	}
 	
