2d6829db943a28d4446f5c201a5fe6ea5bd76694
komalh <komalh@ULTP_649.synerzipune.local>
2017-07-21 17:30:46 +0530

    adding

---
+++ src/test/java/com/testng/testcases/ListnerTest.java
@@ -0,0 +1,60 @@
+package com.testng.testcases;
+
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import com.fb.testcases.CommonUtility;
+
+
+
+@Listeners(com.testng.testcases.TestNGListner.class)
+public class ListnerTest {
+	WebDriver driver;
+	Properties prop;
+	@BeforeMethod()
+	public void setup(){
+		
+      driver=CommonUtility.setupFirefoxBrowser();
+      prop=CommonUtility.readPropertyFile();	
+	  driver.get(prop.getProperty("VerifyTitleURL"));
+		
+	}
+	
+	@Test(enabled=false)
+	public void verifyTitle()
+	
+	{
+		
+		
+		String ActualTitle=driver.getTitle();
+		System.out.println("Title of current webpage: "+ActualTitle);
+	
+		
+	}
+	
+	@Test
+	public void currentURL(){
+		System.out.println("Current URL :" +driver.getCurrentUrl());
+	}
+  
+	
+	@Test
+	public void failedtestcase(){
+		
+		System.out.println("Testing failed test case");
+		Assert.assertTrue(false);
+	}
+	
+	@AfterMethod
+	public void closeApplication(){
+		CommonUtility.tearDown();
+	}
+
+
+
+}
