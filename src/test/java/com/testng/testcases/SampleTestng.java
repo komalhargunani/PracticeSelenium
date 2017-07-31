6ca28f47a4c02c154a4e5f5150813a0999b2f40b
komalh <komalh@ULTP_649.synerzipune.local>
2017-07-31 16:31:03 +0530

    add

--- src/test/java/com/testng/testcases/SampleTestng.java
+++ src/test/java/com/testng/testcases/SampleTestng.java
@@ -3,7 +3,6 @@ package com.testng.testcases;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
